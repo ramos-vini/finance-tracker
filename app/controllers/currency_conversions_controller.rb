17cc9f15e09d98b7dbcdaa617346c39306a7d480
ramos-vini <ramoscontrole@gmail.com>
2025-02-25 16:55:55 -0300

    display currency_conversions on my portfolio

--- app/controllers/currency_conversions_controller.rb
+++ app/controllers/currency_conversions_controller.rb
@@ -1,6 +1,7 @@
 class CurrencyConversionsController < ApplicationController
   def search
     @currency_conversion = CurrencyConversion.fetch_and_create(params[:code], params[:codein])
+    @currency_conversions = current_user.currency_conversion
 
     if !@currency_conversion
       flash[:alert] = "Please enter valid currency codes."
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
   def my_portfolio
+    @currency_conversions = current_user.currency_conversion
   end
 end
