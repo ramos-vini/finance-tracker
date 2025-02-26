0a90f65f5f228d219e2ac6bdeb6b13a1713bae5e
ramos-vini <ramoscontrole@gmail.com>
2025-02-26 19:39:59 -0300

    Display on UI which currency_conversion has been added to Portfolio

--- app/controllers/user_currency_conversions_controller.rb
+++ app/controllers/user_currency_conversions_controller.rb
@@ -3,10 +3,12 @@ class UserCurrencyConversionsController < ApplicationController
     @user_currency_conversion = UserCurrencyConversion.new(user_id: params[:user_id],
                                   currency_conversion_id: params[:currency_conversion_id])
 
+    currency_conversion_code = CurrencyConversion.find(params[:currency_conversion_id]).conversion_code
+                                  
     if @user_currency_conversion.save
-      flash[:notice] = "Currency Conversion was successfully added to Portfolio."
+      flash[:notice] = "#{currency_conversion_code} was successfully added to Portfolio."
     else
-      flash[:alert] = "Currency Conversion could not be added to Portfolio."
+      flash[:alert] = "#{currency_conversion_code} could not be added to Portfolio."
     end
 
     redirect_to "/my_portfolio", allow_other_host: true 
