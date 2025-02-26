15792d6d10101610adbac13421696eaaf48955ad
ramos-vini <ramoscontrole@gmail.com>
2025-02-26 20:16:55 -0300

    Limit each user to only track up to 10 CCs (controller and view restriction)

--- app/controllers/user_currency_conversions_controller.rb
+++ app/controllers/user_currency_conversions_controller.rb
@@ -1,14 +1,21 @@
 class UserCurrencyConversionsController < ApplicationController
   def create
-    @user_currency_conversion = UserCurrencyConversion.new(user_id: params[:user_id],
-                                  currency_conversion_id: params[:currency_conversion_id])
+    user = User.find(params[:user_id])
+    currency_conversion = CurrencyConversion.find(params[:currency_conversion_id])
 
-    currency_conversion_code = CurrencyConversion.find(params[:currency_conversion_id]).conversion_code
-                                  
-    if @user_currency_conversion.save
-      flash[:notice] = "#{currency_conversion_code} was successfully added to Portfolio."
+    if user.currency_conversion.count < 10
+      @user_currency_conversion = UserCurrencyConversion.new(user_id: params[:user_id],
+                                    currency_conversion_id: params[:currency_conversion_id])
+
+      currency_conversion_code = currency_conversion.conversion_code
+                                    
+      if @user_currency_conversion.save
+        flash[:notice] = "#{currency_conversion_code} was successfully added to Portfolio."
+      else
+        flash[:alert] = "#{currency_conversion_code} could not be added to Portfolio."
+      end
     else
-      flash[:alert] = "#{currency_conversion_code} could not be added to Portfolio."
+      flash[:alert] = "You can only track up to 10 Currency Conversions."
     end
 
     redirect_to "/my_portfolio", allow_other_host: true 
