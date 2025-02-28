bca39e3a659c824c4fbd038f7f7242348f3d90a9
ramos-vini <ramoscontrole@gmail.com>
2025-02-27 21:47:53 -0300

    restrict UserCurrencyConversion create and destroy actions to current user

--- app/controllers/user_currency_conversions_controller.rb
+++ app/controllers/user_currency_conversions_controller.rb
@@ -1,36 +1,53 @@
 class UserCurrencyConversionsController < ApplicationController
-  # TODO: Check for current_user permissions
   def create
-    user = User.find(params[:user_id])
-    currency_conversion = CurrencyConversion.find(params[:currency_conversion_id])
+    if is_current_user(params[:user_id])
+      user = User.find(params[:user_id])
+      currency_conversion = CurrencyConversion.find(params[:currency_conversion_id])
 
-    if user.currency_conversion.count < 10
-      @user_currency_conversion = UserCurrencyConversion.new(user_id: params[:user_id],
-                                    currency_conversion_id: params[:currency_conversion_id])
+      if user.currency_conversion.count < 10
+        @user_currency_conversion = UserCurrencyConversion.new(user_id: params[:user_id],
+                                      currency_conversion_id: params[:currency_conversion_id])
 
-      currency_conversion_code = currency_conversion.conversion_code
-                                    
-      if @user_currency_conversion.save
-        flash[:notice] = "#{currency_conversion_code} was added to your Portfolio."
+        currency_conversion_code = currency_conversion.conversion_code
+                                      
+        if @user_currency_conversion.save
+          flash[:notice] = "#{currency_conversion_code} was added to your Portfolio."
+        else
+          flash[:alert] = "#{currency_conversion_code} could not be added to your Portfolio."
+        end
       else
-        flash[:alert] = "#{currency_conversion_code} could not be added to your Portfolio."
+        flash[:alert] = "You can only track up to 10 Currency Conversions."
       end
     else
-      flash[:alert] = "You can only track up to 10 Currency Conversions."
+      flash[:alert] = "You don't have sufficient permissions to perform this action."
     end
 
     redirect_to "/my_portfolio", allow_other_host: true 
   end
 
   def destroy
-    @user_currency_conversion = UserCurrencyConversion.find(params[:id])
+      @user_currency_conversion = UserCurrencyConversion.find(params[:id])
 
-    if @user_currency_conversion
-      currency_conversion = CurrencyConversion.find(@user_currency_conversion.currency_conversion_id)
+      if @user_currency_conversion && is_current_user(@user_currency_conversion.user_id)
+        currency_conversion = CurrencyConversion.find(@user_currency_conversion.currency_conversion_id)
 
-      @user_currency_conversion.destroy
-      flash[:notice] = "#{currency_conversion.conversion_code} was removed from your Portfolio."
-      redirect_to "/my_portfolio"
+        @user_currency_conversion.destroy
+        flash[:notice] = "#{currency_conversion.conversion_code} was removed from your Portfolio."
+      else
+        flash[:alert] = "You don't have sufficient permissions to perform this action."
+      end
+
+    redirect_to "/my_portfolio"
+  end
+
+  private
+
+  # TODO: Check for current_user permissions
+  def is_current_user(user_id)
+    if current_user.id == user_id.to_i
+      return true
+    else
+      return false
     end
   end
 end
